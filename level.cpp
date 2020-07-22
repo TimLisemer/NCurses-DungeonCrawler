abbd52816d160f8ef8d35027e7111697c3f3a020
Tim Lisemer <38297170+TimLisemer@users.noreply.github.com>
2020-07-22 10:26:13 +0200

    Update level.cpp

--- level.cpp
+++ level.cpp
@@ -8,7 +8,7 @@ inline bool instanceof(const T*) {
 Level::Level(UserInterface* ui) {
 
     //Map Insert
-    string LevelPath = "/home/linus/Documents/Ncurses-DungeonCrawler/5-studi.map";
+    string LevelPath = "../Ncurses-DungeonCrawler-master/5-studi.map";
     ifstream file(LevelPath);
     if (!file.good()) {
         throw std::invalid_argument("Level File not found! --- Path = " + LevelPath);
